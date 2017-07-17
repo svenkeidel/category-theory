900856b4ce6a245b20c306c5952cee950d718fc4
Sven Keidel <svenkeidel@gmail.com>
2017-07-18 01:47:15 +0200

    update nix file

--- default.nix
+++ default.nix
@@ -1,19 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  texEnv = pkgs.texlive.combine {
-      inherit (pkgs.texlive) scheme-small latexmk pgf tikz-cd;
-  };
-  
-  hsEnv = pkgs.haskellPackages.ghcWithPackages(p: with p; [
-    Cabal cabal-install hlint comonad
-  ]);
-
-in pkgs.stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
   name = "categories";
   version = "0.0.1";
   src = ./.;
-  buildInputs = [
-    texEnv hsEnv
+  buildInputs = with pkgs; [
+    coq_8_6
   ];
 }
